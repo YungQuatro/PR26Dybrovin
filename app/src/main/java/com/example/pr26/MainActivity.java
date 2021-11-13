c8bd0a6d3f6ba9abbf91b8f5fa956c07c511d091
YungQuatro <specialtanki33@gmail.com>
2021-11-14 00:14:37 +0700

    1
    asd

---
+++ app/src/main/java/com/example/pr26/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.pr26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+        Button btnWeb;
+        Button btnMap;
+        Button btnCall;
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            btnWeb = (Button) findViewById(R.id.btnWeb);
+            btnMap = (Button) findViewById(R.id.btnMap);
+            btnCall = (Button) findViewById(R.id.btnCall);
+
+            btnWeb.setOnClickListener(this);
+            btnMap.setOnClickListener(this);
+            btnCall.setOnClickListener(this);
+        }
+
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()) {
+                case R.id.btnWeb:
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/yungquatro"));
+                    startActivity(intent);
+                    break;
+                case R.id.btnMap:
+                    intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("geo:55.754283,37.62002"));
+                    startActivity(intent);
+                    break;
+                case R.id.btnCall:
+                    intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:12345"));
+                    startActivity(intent);
+                    break;
+            }
+        }
+    }
